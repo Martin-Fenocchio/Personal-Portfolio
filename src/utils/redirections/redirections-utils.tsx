488a794a761e7ad4b318bed3620d357f07571c60
Martín Fenocchio <fenomartin6@gmail.com>
2025-03-04 01:31:50 -0300

    feat: add Open Graph meta tags and title for RedirectionsToRapidamente component

--- src/utils/redirections/redirections-utils.tsx
+++ src/utils/redirections/redirections-utils.tsx
@@ -1,11 +1,29 @@
 import { useEffect } from "react";
+import { Helmet } from "react-helmet-async";
 
 export function RedirectionsToRapidamente() {
   useEffect(() => {
     window.location.href = "https://martin-fenocchio.github.io/rapidamente/";
   }, []);
 
-  return <div></div>;
+  return (
+    <>
+      <Helmet>
+        <meta
+          property="og:url"
+          content="https://martinfenocchio.com/rapidamente"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Rapidamente | Martín Fenocchio" />
+        <meta
+          property="og:description"
+          content="Entrena tu mente y compite con otros en simples pero
+      rápidas cuentas matemáticas."
+        />
+        <title>RápidaMente</title>
+      </Helmet>
+    </>
+  );
 }
 
 export function RedirectionsToBGRemover() {
