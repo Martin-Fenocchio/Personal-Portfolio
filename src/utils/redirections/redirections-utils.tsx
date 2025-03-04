07ddc2912888acfb04ca066950a20214b15a2e8b
Martín Fenocchio <fenomartin6@gmail.com>
2025-03-04 01:43:23 -0300

    feat: enhance SEO for RedirectionsToRapidamente with dynamic meta tags and updated document title

--- src/utils/redirections/redirections-utils.tsx
+++ src/utils/redirections/redirections-utils.tsx
@@ -3,12 +3,39 @@ import { Helmet } from "react-helmet-async";
 
 export function RedirectionsToRapidamente() {
   useEffect(() => {
+    document.title = "Rapidamente - Martín Fenocchio";
+    const metaTags = {
+      description:
+        "Entrena tu mente y compite con otros en simples pero rápidas cuentas matemáticas.",
+      "og:url": "https://martinfenocchio.com/rapidamente",
+      "og:title": "Rapidamente - Martín Fenocchio",
+      "og:description":
+        "Entrena tu mente y compite con otros en simples pero rápidas cuentas matemáticas.",
+    };
+
+    Object.entries(metaTags).forEach(([name, content]) => {
+      let meta =
+        document.querySelector(`meta[property="${name}"]`) ||
+        document.querySelector(`meta[name="${name}"]`);
+
+      if (!meta) {
+        meta = document.createElement("meta");
+        if (name.startsWith("og:")) {
+          meta.setAttribute("property", name);
+        } else {
+          meta.setAttribute("name", name);
+        }
+        document.head.appendChild(meta);
+      }
+      meta.setAttribute("content", content);
+    });
+
     window.location.href = "https://martin-fenocchio.github.io/rapidamente/";
   }, []);
 
   return (
     <>
-      <Helmet>
+      <Helmet prioritizeSeoTags>
         <meta
           property="og:url"
           content="https://martinfenocchio.com/rapidamente"
@@ -17,6 +44,7 @@ export function RedirectionsToRapidamente() {
         <meta property="og:title" content="Rapidamente | Martín Fenocchio" />
         <meta
           property="og:description"
+          key="og:desc"
           content="Entrena tu mente y compite con otros en simples pero
       rápidas cuentas matemáticas."
         />
