780396c342a6c2e36e7198e0697a4d9fa204baf8
Martín Fenocchio <fenomartin6@gmail.com>
2025-04-10 00:11:48 -0300

    feat: migrate project images to S3

--- src/components/home/projects/project-item.tsx
+++ src/components/home/projects/project-item.tsx
@@ -10,10 +10,15 @@ interface ProjectItemProps {
 }
 
 function ProjectItem(props: ProjectItemProps) {
+  console.log("import.meta.env", import.meta.env);
+
   return (
     <a href={props.url} target="BLANK">
       <article className="project-item" data-has-url={props.url != null}>
-        <img src={props.image} alt="project image" />
+        <img
+          src={import.meta.env.VITE_IMAGES_URL + props.image}
+          alt="project image"
+        />
 
         <h3>{props.title}</h3>
         {props.description}
